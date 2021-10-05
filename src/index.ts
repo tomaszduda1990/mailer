2bd21d8bcb12fd28957d05feb88483250e4f2028
Tomasz Duda <tduda@online.saba.com>
2021-10-05 16:41:09 +0200

    added google auth, first part

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import userRouter from './router/user';
 import authRouter from './router/auth';
 
-const app = express();
+const app: express.Application = express();
 
 app.use('/auth', authRouter);
 app.use('/user', userRouter);
 app.use('/', (req, res) => {
   res.json('opening page');
 });
-// comment is nice
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,24 +1,33 @@
 import express from 'express';
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-
+import { config } from '../config/private';
 const authRouter: express.Router = express.Router();
 // get login page
 
-passport.use(new GoogleStrategy());
-authRouter.get('/google', (req, res, next) => {
-  const { apiKey } = req.query;
-  if (!apiKey) {
-    res.send({ msg: 'GET Google' });
-    next();
-  }
-  res.send(apiKey);
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.googlePublicKey,
+      clientSecret: config.googlePrivateKey,
+      callbackURL: '/auth/google/callback',
+      passReqToCallback: true,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('Profile', profile);
+      console.log('done', done);
+    }
+  )
+);
+authRouter.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
 
-// post login to google
-authRouter.post('/google', (req, res) => {
-  console.log('post google');
-  res.send({ msg: 'POST ROUTE' });
-});
+authRouter.get('/google/callback', passport.authenticate('google'));
 
 export default authRouter;
